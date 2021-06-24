7de17ec69c33b2fe4e966d83a4d1180f36971052
bierb <bierbrauerdaniel@web.de>
2021-06-24 12:47:09 +0200

    Javadoc beschreibungen

--- Praktikum 2 INGIJ/src/Bestenliste.java
+++ Praktikum 2 INGIJ/src/Bestenliste.java
@@ -9,15 +9,44 @@ import javax.swing.JPanel;
  */
 public interface Bestenliste // extends JPanel
 {
+	/**
+	 * Bestätigt per Mausklick die Eingabe des Spielernames .
+	 * 
+	 * @param mouseListener Übergabe des jeweiligen Maus-Events
+	 */
 	public void Name_eingegeben(MouseListener mouseListener);
-
+	
+	/**
+	 * Der Start eines neuen Spiels wird per Mausklick eingeleitet.
+	 * 
+	 * @param mouseListener Übergabe des jeweiligen Maus-Events
+	 */
 	public void neues_Spiel(MouseListener mouseListener);
-
-	public void neues_Erbebnis(int folgenlaenge, int spielzeit);
-
+	
+	/**
+	 * Erstellt ein neues Ergebnis mit folgenlänge und spielzeit des letzten Spiels.
+	 * 
+	 * @param folgenlaenge Länge der Richtig geratenen Ziffern
+	 * @param spielzeit
+	 */
+	public void neues_Ergebnis(int folgenlaenge, int spielzeit);
+	
+	/**
+	 * Öffnet die Texteingabe zum eintragen des Spielernames.
+	 * 
+	 */
 	public void aktiviere_Namenseingabe();
-
+	
+	/**
+	 * Gibt die Bestenliste der Letzten Spiele aus. 
+	 * 
+	 */
 	public void zeige_Liste_an();
-
+	
+	/**
+	 * Sorgt mit übergebenem boolean wert für die Sichtbarkeit der Liste.
+	 * 
+	 * @param wert True/False für eine sichtbare/unsichtbare Liste
+	 */
 	public void sichtbar(boolean wert);
 }
--- Praktikum 2 INGIJ/src/BestenlisteZeile.java
+++ Praktikum 2 INGIJ/src/BestenlisteZeile.java
@@ -5,5 +5,10 @@
  */
 public interface BestenlisteZeile //extends JPanel
 {
+	/**
+	 * Zeigt eine Zeile der Bestenliste (Spielergebnis) an.
+	 * 
+	 * @param ergebnis Ergebniswerte des Spieldurchgangs
+	 */
 	public void zeige_an(Ergebnis ergebnis);
 }
