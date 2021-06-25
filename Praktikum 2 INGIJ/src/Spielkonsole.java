eef2923c7133eb932aff283838048fe15d1dc770
JDK98 <82526029+JDK98@users.noreply.github.com>
2021-06-25 13:55:42 +0200

    purpose changes

--- Praktikum 2 INGIJ/src/Spielkonsole.java
+++ Praktikum 2 INGIJ/src/Spielkonsole.java
@@ -4,20 +4,46 @@
  *
  */
 public interface Spielkonsole {
+	/**
+	 * gibt an, ob Wert sichtbar ist
+	 * @param wert
+	 */
 	public void sichtbar(boolean wert);
-
+	/**
+	 * gibt die Ziffernfolge aus
+	 */
 	public void starte_Praesentation_Ziffernfolge();
-
+	/**
+	 * startet neue Ziffernfolge
+	 */
 	public void beginne_neue_Ziffernfolge();
-
+	/**
+	 * zeigt die eingegebene Ziffer an
+	 * @param ziffer
+	 */
 	public void Ziffer_ausgewaehlt(Ziffer ziffer);
-
+	/**
+	 * überprüft, ob die eingegebene Ziffer mit der der Ziffernfolge übereinstimmt
+	 * @param ziffer
+	 * @return boolean
+	 */
 	public boolean ausgewaehlte_Ziffer_korrekt(Ziffer ziffer);
-
+	/**
+	 * überprüft, ob alle eingegebenen Ziffern korrekt sind
+	 * @return boolean
+	 */
 	public boolean alle_Ziffern_Memoriert();
-
+	/**
+	 * übergibt die Spielzeit in Sekunden
+	 * @return int
+	 */
 	public int Spielzeit();
-
+	/** 
+	 * übergibt die Laenge der korrekt eingegebenen Ziffernfolge
+	 * @return int
+	 */
 	public int Laenge_Ziffernfolge();
 
 }
+
+
