95e6266a2d05833046051803029fca160cdfa5e1
JDK98 <82526029+JDK98@users.noreply.github.com>
2021-06-25 13:58:52 +0200

    Update Zustand.java

--- Praktikum 2 INGIJ/src/Zustand.java
+++ Praktikum 2 INGIJ/src/Zustand.java
@@ -4,17 +4,33 @@
  *
  */
 public interface Zustand {
+	/**
+	 * startet das Spiel
+	 */
 	public void Spiel_gestartet();
-
+	/**
+	 * beendet die Praesentation der Ziffernfolge
+	 */
 	public void Praesentation_Ziffernfolge_beendet();
-
+	/**
+	 * zeigt die aktuell ausgewählte Ziffer an
+	 * @param ziffer
+	 */
 	public void Ziffer_ausgewaehlt(Ziffer ziffer);
-
+	/**
+	 * startet ein neues Spiel
+	 */
 	public void neues_Spiel();
-
+	/**
+	 * erzeugt die Namenseingabe
+	 */
 	public void Name_eingegeben();
-
+	/**
+	 * Eintritt in einen Zustand 
+	 */
 	public void entry();
-
+	/**
+	 * Ausgang eines Zustandes
+	 */
 	public void exit();
 }
