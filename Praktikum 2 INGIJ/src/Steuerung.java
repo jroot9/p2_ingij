1e0ceee2d91e4c67b2af32a6091b4d4cbbbff84b
JDK98 <82526029+JDK98@users.noreply.github.com>
2021-06-25 13:56:53 +0200

    Update Steuerung.java

--- Praktikum 2 INGIJ/src/Steuerung.java
+++ Praktikum 2 INGIJ/src/Steuerung.java
@@ -4,14 +4,27 @@
  *
  */
 public interface Steuerung {
+	/**
+	 * startet das Spiel
+	 */
 	public void Spiel_gestartet();
-
+	/**
+	 * beendet die Anzeige der Ziffernfolge
+	 */
 	public void Praesentation_Ziffernfolge_beendet();
-
+	/**
+	 * zeigt die aktuell ausgewählte Ziffer an
+	 * @param ziffer
+	 */
 	public void Ziffer_ausgewaehlt(Ziffer ziffer);
-
+	/**
+	 * startet ein neues Spiel
+	 */
 	public void neues_Spiel();
-
+	/**
+	 * ermöglicht die Nameneingabe
+	 */
 	public void Name_eingegeben();
 
 }
+
