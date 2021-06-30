f2d0afa92e5d599c0afe8152c8b4a82a5a9928f5
jroot9 <64260382+jroot9@users.noreply.github.com>
2021-06-30 11:03:45 +0200

    Update Ergebnis.java

--- Praktikum 2 INGIJ/src/Ergebnis.java
+++ Praktikum 2 INGIJ/src/Ergebnis.java
@@ -5,14 +5,16 @@
  */
 public interface Ergebnis extends Comparable{
 	/*
+	 * Konstruktor
 	 * @param String name
-	 * @param int zeit
-	 * @param int ziffernanzahl
-	 */
+	 * @param int spielzeitInSekunden
+	 * @param int reihenlaenge
+	 * 
+	 * public Ergebnis(String name, int spielzeitInSekunden, int reihenlaenge); 
 	/**
-	 * Method to compare one Ergebnis Object to another
+	 * Vertgleiche das aktuelle Ergebnis mit einem übergebenen Ergebnis Objekt e, dann wird ein Integer mit dem Ergebnis zurückgegeben (0 kleiner, 1 groesser, 2 gleich)
 	 * @param e
-	 * @return
+	 * @return compareWertAlsInt
 	 */
 	public int CompareTo(Ergebnis e);
 }
